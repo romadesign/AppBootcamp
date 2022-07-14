6e7bda0f55f977e3584788851943b7f7126294c3
romacode <roman247.rg@gmail.com>
2022-07-14 12:53:17 +0200

    css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="container">
+  <div class="row">
     <section class="content_general">
       <div class="sub_content row d-flex align-items-center justify-content-center">
         <div class="content_description col-md-6">
@@ -10,11 +11,13 @@
             <h2 class="fs-5">Las empresas necesitan nuevo talento digital. Ofrecen buenos salarios, contratos estables y desarrollo profesional</h2>
             <button type="button" class="btn btn-success">Cursos</button>
             <div class="content_follow">
-              <h4>Follow</h4>
-              <div class="">
-                <i>LInkendn</i>
-                <i>Face</i>
-                <i>Instagram</i>
+              <h4>Síguenos</h4>
+              <div class="content_icons">
+                <img src="/image/googlemaps.svg" alt="">  
+                <img src="/image/facebook.svg" alt="">  
+                <img src="/image/instagram.svg" alt="">  
+                <img src="/image/twitter.svg" alt="">  
+                <img src="/image/googlemessages.svg" alt="">  
               </div>
             </div>
           </div>
@@ -31,9 +34,9 @@
     </section>
     <section class="content_information">
       <div class="row content_type_formation">
-        <div class="card desription_information">
+        <div class="card shadow p-3 mb-5 bg-body rounded desription_information">
           <img src="/image/img1.svg" alt="">
-          <h3 class="fw-bold">Formato intensivo</h3>
+          <h3 class="fw-bold text-center">Formato intensivo</h3>
          <ul>
             <li>Proyectos reales.</li>
             <li>Las tecnologías demandadas por las empresas.</li>
@@ -41,22 +44,51 @@
             <li>Competencias técnicas y laborales.</li>
           </ul>
         </div>
-        <div class="card desription_information">
+        <div class="card shadow p-3 mb-5 bg-body rounded desription_information">
           <img src="/image/img2.svg" alt="">
-          <h3 class="fw-bold">Sin barreras</h3>
+          <h3 class="fw-bold text-center">Sin barreras</h3>
          <ul>
             <li>Formación gratuita de calidad.</li>
             <li>Dirigida especialmente a personas con dificultades de inserción laboral.</li>
             <li>Sin necesidad de conocimientos previos.</li>
           </ul>
         </div>
-        <div class="card desription_information">
+        <div class="card shadow p-3 mb-5 bg-body rounded desription_information">
           <img src="/image/img3.svg" alt="">
-          <h3 class="fw-bold">Enfocada al empleo</h3>
+          <h3 class="fw-bold text-center">Enfocada al empleo</h3>
          <ul>
             <li>Salida positiva del 100%.</li>
             <li>Practicas en empresas del sector.</li>
             <li>Seguimiento de los progresos.</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section class="row content_cards_general">
+      <div class="col-md-6">
+        <h3 class="fw-bold p-3 text-center">Acepta el reto y participa</h3>
+        <div class="conten_img">
+          <img src="/image/img5.svg" alt="">
+        </div>
+      </div>
+      <div class="col-md-6 content_import ">
+        <div class="shadow p-3 mb-5 bg-body rounded">
+          <h4 class="text-center fw-bold ">No importa</h4>
+          <ul>
+            <li>Tus conocimientos previos</li>
+            <li>El nivel de estudios</li>
+            <li>Tus informes académicos</li>
+            <li>Tus empleos anteriores</li>
+            <li>Que no tengas dinero</li>
+          </ul>
+        </div>
+        <div class="shadow p-3 mb-5 bg-body rounded">
+          <h4 class="text-center fw-bold ">Nos interesa</h4>
+          <ul>
+            <li>Tu motivación</li>
+            <li>Tu lógica</li>
+            <li>Tu autonomía </li>
+            <li>Tu capacidad de trabajo en equipo</li>
           </ul>
         </div>
       </div>
@@ -67,7 +99,7 @@
           <h3 class="fw-bold text-center p-3">Nuestro cursos</h3>
         </div>
         <div class="content_courses">
-          <div class="card content_course">
+          <div class="card content_course shadow p-3 mb-5 rounded">
             <div>
               <img src="/image/img3.svg" alt="">
             </div>
@@ -89,7 +121,7 @@
               <button type="button" class="btn btn-success">Más información</button>
             </div>
           </div>
-          <div class="card content_course">
+          <div class="card content_course shadow p-3 mb-5 rounded">
             <div>
               <img src="/image/img3.svg" alt="">
             </div>
@@ -111,7 +143,7 @@
               <button type="button" class="btn btn-success">Más información</button>
             </div>
           </div>
-          <div class="card content_course">
+          <div class="card content_course shadow p-3 mb-5 rounded">
             <div>
               <img src="/image/img3.svg" alt="">
             </div>
@@ -136,31 +168,7 @@
         </div>
       </div>
     </section>
-    <section class="content_cards_general">
-      <div class="">
-          <h3 class="fw-bold text-center p-3">Acepta el reto y participa</h3>
-      </div>
-      <div class="row d-flex justify-content-center">
-        <div class="card_content col-md-6">
-          <h4>No importa</h4>
-          <ul>
-            <li>Tus conocimientos previos</li>
-            <li>El nivel de estudios</li>
-            <li>Tus informes académicos</li>
-            <li>Tus empleos anteriores</li>
-            <li>Que no tengas dinero</li>
-          </ul>
-        </div>
-        <div class="card_content col-md-6">
-          <h4>Nos interesa</h4>
-          <ul>
-            <li>Tu motivación</li>
-            <li>Tu lógica</li>
-            <li>Tu autonomía </li>
-            <li>Tu capacidad de trabajo en equipo</li>
-          </ul>
-        </div>
-      </div>
-    </section>
-
+   
+  </div>
+</div>
 @endsection
